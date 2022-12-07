1f32ee549b4e0042f0e2a6ba7e1e9d546b6be472
Andreas Denger <a.denger@outlook.de>
2022-12-07 14:54:38 +0100

    trembl notebook

--- preprocessing/filter_go.py
+++ preprocessing/filter_go.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 header = [
     "db",
     "db_object_id",
@@ -18,7 +20,14 @@ header = [
     "gene_product_form_id",
 ]
 outfile_header = ["Uniprot", "qualifier", "go_id", "evidence_code", "aspect", "date"]
-with open("/home/ad/gene_ontology/goa_uniprot_all_filtered.gaf", "w") as outfile:
+
+current_uniprot_proteins = set(
+    pd.read_table(
+        "/home/ad/uniprot_2022_04/uniprot_evidence_go_kw.tsv", index_col=0
+    ).index.str.strip().to_list()
+)
+
+with open("/home/ad/gene_ontology/goa_uniprot_all_filtered3.gaf", "w") as outfile:
     outfile.write("\t".join(outfile_header) + "\n")
     with open("/home/ad/gene_ontology/goa_uniprot_all.gaf") as infile:
         for line in infile:
@@ -26,8 +35,7 @@ with open("/home/ad/gene_ontology/goa_uniprot_all_filtered.gaf", "w") as outfile
                 # also takes care of comments
                 continue
             items = line.split("\t")
-            outfile.write(
-                "\t".join([items[i] for i in (1,3,4,6,8,13)]+["\n"])
-            )
+            if items[1].strip() in current_uniprot_proteins:
+                outfile.write("\t".join([items[i].strip() for i in (1, 3, 4, 6, 8, 13)] + ["\n"]))
 
 print("done.")
