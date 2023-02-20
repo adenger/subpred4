58010a98c45e04528c426b7280c97fff215bd89b
Andreas Denger <a.denger@outlook.de>
2023-02-20 17:53:16 +0100

    added new file

--- subpred/chebi.py
+++ subpred/chebi.py
@@ -1,7 +1,6 @@
 from .ontology import Ontology
 
 class ChebiOntology(Ontology):
-    # TODO parser for object properties
     def encode_identifier(self, identifier: str):
         return identifier.replace(":", "_", 1)
 
--- subpred/go_utils.py
+++ subpred/go_utils.py
@@ -33,7 +33,6 @@ GO_FILE = "../data/raw/ontologies/go.owl"
 
 
 class GeneOntology(Ontology):
-    # TODO parser for object properties
     def encode_identifier(self, identifier: str):
         return identifier.replace(":", "_", 1)
 
