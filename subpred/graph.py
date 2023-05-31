903cda4c78272884d5a55e2c382a047e29bc876d
Andreas Denger <a.denger@outlook.de>
2023-05-31 10:57:24 +0200

    graph

---
+++ subpred/graph.py
@@ -0,0 +1,12 @@
+
+
+def get_protein_dataset(organism_ids:set):
+    pass
+
+
+
+def get_substrate_graph(organism_ids:set):
+    df_uniprot_organisms = get_protein_dataset(organism_ids)
+    # TODO well-defined goal
+
+    pass
