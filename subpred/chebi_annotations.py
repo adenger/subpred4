ad84c9302989dd697f9451e213584a73a3992b8d
Andreas Denger <a.denger1@gmail.com>
2023-08-08 15:16:48 +0200

    formatting

--- subpred/chebi_annotations.py
+++ subpred/chebi_annotations.py
@@ -1,4 +1,6 @@
 from subpred.util import load_df
+
+
 def get_id_update_dict(graph):
     id_update_dict = dict()
     for term, alt_ids in graph.nodes(data="alt_id"):
@@ -11,11 +13,12 @@ def get_id_update_dict(graph):
         id_update_dict[term] = term
     return id_update_dict
 
+
 def get_go_chebi_annotations(
     dataset_path: str,
     go_ids_subset: set = None,
     go_chebi_relations_subset: set = {"has_primary_input", "has_participant"},
-    filter_by_3star:bool = False
+    filter_by_3star: bool = False,
 ):
     df_go_chebi = load_df("go_chebi", folder_path=dataset_path)
     graph_chebi = load_df("chebi_obo", folder_path=dataset_path)
@@ -41,14 +44,18 @@ def get_go_chebi_annotations(
         df_go_chebi = df_go_chebi[df_go_chebi.go_id.isin(go_ids_subset)]
 
     if filter_by_3star:
-        chebi_3star = {node for node, subset in graph_chebi.nodes(data="subset") if "3_STAR" in subset}
+        chebi_3star = {
+            node
+            for node, subset in graph_chebi.nodes(data="subset")
+            if "3_STAR" in subset
+        }
         df_go_chebi = df_go_chebi[df_go_chebi.chebi_id.isin(chebi_3star)]
+
+    go_id_to_term = {go_id: go_term for go_id, go_term in graph_go.nodes(data="name")}
+    df_go_chebi.insert(1, "go_term", df_go_chebi.go_id.map(go_id_to_term))
 
     df_go_chebi = df_go_chebi.sort_values(
         ["go_id", "chebi_id", "chebi_go_relation"]
     ).reset_index(drop=True)
 
     return df_go_chebi
-
-
-
