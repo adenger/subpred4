07ec36c09aa462271b5b94643e0a9ba53f33f486
Andreas Denger <a.denger1@gmail.com>
2023-08-30 19:54:40 +0200

    refactoring

---
+++ subpred/go_semantic_similarity.py
@@ -0,0 +1,179 @@
+# https://rpy2.github.io/doc/latest/html/introduction.html
+# https://berkeley-scf.github.io/tutorial-parallelization/parallel-R.html
+
+from tempfile import NamedTemporaryFile
+from rpy2.robjects import r, StrVector, packages, pandas2ri
+import rpy2.robjects as ro
+import numpy as np
+import pandas as pd
+from subpred.util import load_df
+
+def get_go_data_df(
+    organism_ids: set,
+    aspect: str,
+):
+    """
+        The GoSemSim package expects data from AnnotationDBI.
+        However, this means that we can only calculate similarities between GO terms from the same organism, not on the entire GO.
+        We fix this by creating this object directly, from our go annotation pickle, analogous to the godata function in GoSemSim_summary_.
+        The original code does not use the GO ancestors (GOALL), only direct annotations (GO).
+        It adds the ancestors later during calculation if they are needed.
+        If an AnnotationDbi object is available then we use that.
+    Args:
+        organism_ids (set): Organism ids to take annotated proteins from
+        aspect (str): Sub-ontology, one of C, F, P. Defaults to F
+
+    Returns:
+        pd.DataFrame: Annotation dataframe compatible with get_go_semantic_similarities
+    """
+    df_uniprot_org = load_df("uniprot")
+    df_uniprot_org = df_uniprot_org[
+        df_uniprot_org.organism_id.isin(organism_ids)
+    ]  # TODO filter for more?
+    protein_set = set(df_uniprot_org.index)
+
+    df_go_data_semsim = load_df("go")
+    df_go_data_semsim = df_go_data_semsim[
+        (df_go_data_semsim.Uniprot.isin(protein_set))
+        # & (~df_go_data_semsim.evidence_code.isin(evidence_codes_exclude))
+        & (df_go_data_semsim.aspect == aspect)
+    ].reset_index(drop=True)
+    aspect_to_orgdb_aspect = {"F": "MF", "C": "CC", "P": "BP"}
+    df_go_data_semsim["aspect"] = df_go_data_semsim.aspect.map(aspect_to_orgdb_aspect)
+    df_go_data_semsim = df_go_data_semsim.drop("qualifier", axis=1).rename(
+        columns={
+            "Uniprot": "UNIPROT",
+            "go_id": "GO",
+            "evidence_code": "EVIDENCE",
+            "aspect": "ONTOLOGY",
+        }
+    )
+    return df_go_data_semsim
+
+
+def get_go_semantic_similarities(
+    organism_ids: set,
+    aspect: str,
+    go_id_subset: np.array,
+    method="Wang",  
+    conversion_method: str = "internal",
+    parallelization_method:str="sequential"
+):
+    """Wrapper for the GOSemSim R package, with support for efficient parallel processing
+
+    Args:
+        organism_ids (set): Organism ids to take annotated proteins from
+        aspect (str): Sub-ontology, one of C, F, P
+        go_id_subset (np.array): go ids for which to calculate pairwise similarities
+        method (str, optional):
+            Algorithm for calculation similarity.
+            Options: "Resnik", "Lin", "Rel", "Jiang", "TCSS" and "Wang"
+            Defaults to "Wang". Other methods require IC computation, only implemented for yeast
+        parallelization_method (str, optional):
+            If "multisession", clones the R session for every thread.
+                Slower start and much higher memory consumption. Less likely to crash
+            If "multicore", forks R process and allows parallel access to the dataframes.
+                Global vars *are* copied if they are modified by worker, but we only read them anyways.
+                Much faster and lower memory consumption, not available on Windows.
+                Can cause crashes for some values of parameter "method"
+            If "sequential": No parallelization
+            Defaults to "sequential".
+        conversion_method (str, optional):
+            If "temp_file", write results to tmp file in R and then read that file with pandas, deleting the file afterwards.
+            If "internal", uses internal conversion methods between rpy2 and pandas.
+            Which method is faster depends on the machine and the dataset size.
+            Defaults to "internal".
+    Raises:
+        ValueError: Parameter not recognized
+
+    Returns:
+        _type_: _description_
+    """
+
+    packages.importr("GOSemSim")
+    packages.importr("future.apply")
+    packages.importr("Rcpp")
+
+    r(
+        """
+        read_go_data <- function(path){
+            goa <- read.table(path)
+            kk <- unique(goa$UNIPROT)
+            d <- new("GOSemSimDATA", keys = kk, ont = "MF", geneAnno = goa)
+            return(d)
+        }
+        similarities <- function(go1, go2){
+            goSim(go1, go2, semData=d, measure="Wang")
+        }
+        similarities_vec <- function(go_term, go_vec, measure, go_data){
+            similarities_list <- lapply(go_vec, goSim, GOID2=go_term, semData=go_data, measure=measure)
+            return(unlist(similarities_list))
+        }
+
+        similarities_par <- function(go_vec, method, go_data, plan="sequential"){
+            if(plan == "sequential"){
+                plan(sequential)
+            } else if (plan == "multicore"){
+                plan(multicore)
+            } else if (plan == "multisession"){
+                plan(multisession)
+            }
+            results <- future_lapply(go_vec, similarities_vec, go_vec=go_vec, measure=method, go_data=go_data)
+            # results <- lapply(go_vec, similarities_vec, go_vec=go_vec, measure=method, go_data=go_data)
+            results <- as.data.frame(results, row.names = go_vec, col.names = go_vec)
+            return(results)
+        }
+        write_tmp <- function(df, file_name){
+            write.table(df, file_name, sep='\t', quote=FALSE, col.names=TRUE, row.names=TRUE)
+        }
+    """
+    )
+    if organism_ids == {559292}:  # s. cerevisiae
+        packages.importr("org.Sc.sgd.db")
+        godata_func = r["godata"]
+        go_data_r = godata_func(
+            "org.Sc.sgd.db",
+            # keytype="ENTREZID",
+            ont="MF",
+            computeIC=method != "Wang",
+            processTCSS=method == "TCSS",
+        )
+    else:
+        # Workaround for other datasets:Transform the data to what the R package expects
+        print("Warning: organism not found. creating custom GO dataset...")
+        df_go_data_semsim = get_go_data_df(organism_ids=organism_ids, aspect=aspect)
+
+        tmp_file = NamedTemporaryFile(suffix=".tsv")
+        df_go_data_semsim.to_csv(tmp_file, sep="\t")
+        df_goa_path = tmp_file.name
+        go_data_r = r["read_go_data"](df_goa_path)
+
+        tmp_file.close()
+
+    goSimVec = r["similarities_par"]
+    vector = (
+        np.unique(go_id_subset)
+        if go_id_subset is not None
+        else df_go_data_semsim.GO.unique()
+    )
+    vector_r = StrVector(vector)
+    results_r = goSimVec(vector_r, method, go_data_r, plan=parallelization_method)
+
+    if conversion_method == "internal":
+        # df_go_similarity = ro.conversion.rpy2py(results_r)
+        with (ro.default_converter + pandas2ri.converter).context():
+            df_go_similarity = ro.conversion.get_conversion().rpy2py(results_r)
+    elif conversion_method == "temp_file":
+        # write tmp file then read it with pandas. converting the R object to python took longer than the calculations
+        write_table = r["write_tmp"]
+        tmp_file = NamedTemporaryFile()
+        write_table(results_r, tmp_file.name)
+        df_go_similarity = pd.read_table(tmp_file)
+        tmp_file.close()
+    else:
+        raise ValueError("Invalid dataframe conversion method.")
+
+    df_go_similarity.columns = df_go_similarity.columns.to_series().str.replace(
+        ".", ":"
+    )
+    return df_go_similarity
---
+++ subpred/sequence_identity.py
@@ -0,0 +1,178 @@
+from rpy2.robjects import r, packages, StrVector
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+import pandas as pd
+import numpy as np
+
+
+def get_pairwise_sequence_identities(
+    series_sequences: pd.Series,
+    substitution_matrix: str = "BLOSUM62",
+    gap_opening: int = 10,
+    gap_extension: int = 4,
+    identity_calculation_method: str = "PID1",
+) -> pd.DataFrame:
+    """Uses R packages Biostrings and future.apply for fast, parallel computation
+    of pairwise sequence identity scores from optimal global sequence alignments
+    calculated with the Needleman-Wunsch algorithm.
+
+    Args:
+        series_sequences (pd.Series): Series with identifiers as index and sequences as values
+        substitution_matrix (str, optional): PAM or BLOSUM matrix.
+            Options: "BLOSUM45", "BLOSUM50", "BLOSUM62", "BLOSUM80", "BLOSUM100",
+                "PAM30", "PAM40", "PAM70", "PAM120", "PAM250".
+            Defaults to "BLOSUM62".
+        gap_opening (int, optional): penalty for gap opening during alignment. Defaults to 10.
+        gap_extension (int, optional): penalty for gap extension during alignment. Defaults to 4.
+        identity_calculation_method (str, optional):
+            "PID1":100 * (identical positions) / (aligned positions + internal gap positions)
+
+            "PID2":100 * (identical positions) / (aligned positions)
+
+            "PID3":100 * (identical positions) / (length shorter sequence)
+
+            "PID4":100 * (identical positions) / (average length of the two sequences)
+
+            Defaults to "PID1".
+    Returns:
+        pd.DataFrame: Sequence identity scores.
+    """
+    packages.importr("Biostrings")
+    packages.importr("future.apply")
+    r(
+        """
+        pairwiseAlignmentIdentityBatch <- function( 
+            subject, 
+            patterns, 
+            ...
+        ){
+            # to parallelize outer loop
+            alignments <- pairwiseAlignment(
+                pattern=patterns, 
+                subject=subject,
+                ...
+            )
+            return (alignments)  
+        }
+        pairwiseAlignmentsList <- function(
+            sequences,
+            type="global", # Needleman-Wunsch. "local" for smith-waterman
+            substitutionMatrix="BLOSUM62",
+            gapOpening=10, 
+            gapExtension=4
+        ){
+            # TODO would scale better if not re-calculating symmetrical scores
+            plan(multicore)  # uses process fork, on windows use multisession (slower)
+            alignments_list <- future_lapply(
+                sequences, 
+                pairwiseAlignmentIdentityBatch, 
+                patterns=sequences, 
+                type=type, 
+                substitutionMatrix=substitutionMatrix, 
+                gapOpening=gapOpening, 
+                gapExtension=gapExtension
+            )
+            return (alignments_list)
+        }
+        calculateIdentitiesMatrix <- function(alignmentList, accessions, pidType="PID1"){
+            identities <- lapply(alignmentList, pid, type=pidType)
+            df_identities <- as.data.frame(identities, row.names=accessions, col.names=accessions)
+            return (df_identities)
+        }
+        calculateScoresMatrix <- function(alignmentList, accessions){
+            scores <- lapply(alignmentList, score)
+            df_scores <- as.data.frame(scores, row.names=accessions, col.names=accessions)
+            return (df_scores)
+        }
+        
+    """
+    )
+
+    pairwise_alignments_list = r["pairwiseAlignmentsList"]
+    calculate_identities_matrix = r["calculateIdentitiesMatrix"]
+    calculate_scores_matrix = r["calculateScoresMatrix"]
+    # parameter "local" does not make sense here, as local alignments usually have sequence identity of around 100%.
+    # local would only make sense together with score() instead of pid()
+    alignments_list = pairwise_alignments_list(
+        sequences=StrVector(series_sequences.values),
+        substitutionMatrix=substitution_matrix,
+        gapOpening=gap_opening,
+        gapExtension=gap_extension,
+    )
+    df_identity_r = calculate_identities_matrix(
+        alignmentList=alignments_list,
+        accessions=StrVector(series_sequences.index),
+        pidType=identity_calculation_method,
+    )
+    df_scores_r = calculate_scores_matrix(
+        alignmentList=alignments_list, accessions=StrVector(series_sequences.index)
+    )
+
+    with (ro.default_converter + pandas2ri.converter).context():
+        df_protein_identity = ro.conversion.get_conversion().rpy2py(df_identity_r)
+        df_protein_alignment_scores = ro.conversion.get_conversion().rpy2py(df_scores_r)
+
+    return df_protein_identity, df_protein_alignment_scores
+
+
+def get_pairwise_go_scores(
+    go_terms: np.array,
+    go_to_proteins_arr: dict,
+    df_protein_scores: pd.DataFrame,
+    aggr_method: str = "mean",
+):
+    func = {"mean": np.mean, "median": np.median, "min": np.min, "max": np.max}[
+        aggr_method
+    ]
+    records = list()
+    for i, go_term in enumerate(go_terms):
+        # protein arrays have to be unique and sorted!
+        protein_arr1 = go_to_proteins_arr[go_term]
+        for j in range(i, len(go_terms)):
+            go_term_other = go_terms[j]
+            protein_arr2 = go_to_proteins_arr[go_term_other]
+            df_scores_proteinsets = df_protein_scores.loc[protein_arr1, protein_arr2]
+            score = func(df_scores_proteinsets.values)
+            records.append([go_term, go_term_other, score])
+            records.append([go_term_other, go_term, score])
+    df_results = pd.DataFrame(data=records, columns=["go_id1", "go_id2", "score"])
+    df_results = df_results.drop_duplicates()
+    df_results = df_results.pivot(index="go_id1", columns="go_id2", values="score")
+    return df_results
+
+
+def get_pairwise_similarity_matrix(
+    df_uniprot_goa: pd.DataFrame,
+    df_sequences: pd.DataFrame,
+    exclude_iea: bool = True,
+    score: str = "identity",
+    aggr_method: str = "median",
+):
+    df_uniprot_goa_copy = df_uniprot_goa.copy(deep=True)
+    if exclude_iea:
+        df_uniprot_goa_copy = df_uniprot_goa[df_uniprot_goa.evidence_code != "IEA"]
+    # go_ids_unique = sorted(df_uniprot_goa_copy.go_id_ancestor.unique())
+    go_to_proteins_arr = (
+        df_uniprot_goa_copy[["Uniprot", "go_id_ancestor"]]
+        .groupby("go_id_ancestor")
+        .apply(lambda x: np.sort(x.Uniprot.unique().to_numpy()))
+    )
+    series_sequences = (
+        df_sequences.loc[df_uniprot_goa_copy.Uniprot.unique()]
+        .sequence.drop_duplicates()
+        .sort_index()
+    )
+    df_protein_identity, df_protein_alignment_scores = get_pairwise_sequence_identities(
+        series_sequences=series_sequences
+    )
+    go_terms_unique_sorted = np.sort(df_uniprot_goa_copy.go_id_ancestor.unique())
+    df_go_median_sequence_identity = get_pairwise_go_scores(
+        go_terms=go_terms_unique_sorted,
+        go_to_proteins_arr=go_to_proteins_arr,
+        df_protein_scores=df_protein_identity
+        if score == "identity"
+        else df_protein_alignment_scores,
+        aggr_method=aggr_method,
+    )
+
+    return df_go_median_sequence_identity
