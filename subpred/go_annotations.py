dbac9115228ce1f08e8c1b828c7a6aa39003ad5d
Andreas Denger <a.denger1@gmail.com>
2023-09-26 16:30:44 +0200

    added map of explanations

--- subpred/go_annotations.py
+++ subpred/go_annotations.py
@@ -2,6 +2,35 @@ from subpred.util import load_df
 import networkx as nx
 import pandas as pd
 
+EVIDENCE_CODE_TO_DESCRIPTION = {
+    "IMP": "experimental_evidence",
+    "IPI": "experimental_evidence",
+    "IEP": "experimental_evidence",
+    "IDA": "experimental_evidence",
+    "EXP": "experimental_evidence",
+    "IGI": "experimental_evidence",
+    "HDA": "experimental_evidence_high_throughput",
+    "HMP": "experimental_evidence_high_throughput",
+    "HTP": "experimental_evidence_high_throughput",
+    "HGI": "experimental_evidence_high_throughput",
+    "HEP": "experimental_evidence_high_throughput",
+    "IBA": "phylogenetically_inferred",
+    "IBD": "phylogenetically_inferred",
+    "IKR": "phylogenetically_inferred",
+    "IRD": "phylogenetically_inferred",
+    "ISS": "computational_analysis",
+    "ISO": "computational_analysis",
+    "ISA": "computational_analysis",
+    "ISM": "computational_analysis",
+    "IGC": "computational_analysis",
+    "RCA": "computational_analysis",
+    "NAS": "author_statement",
+    "TAS": "author_statement",
+    "IC": "curator_statement",
+    "ND": "curator_statement",
+    "IEA": "electronic_annotation",
+}
+
 
 def get_go_subgraph(
     graph_go,
