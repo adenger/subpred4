20fa452db183f2dfa29619ff07f4cc74f2ba6997
Andreas Denger <a.denger1@gmail.com>
2024-03-13 22:44:08 +0100

    rerun, refactor

--- subpred/overlap_matrix.py
+++ subpred/overlap_matrix.py
@@ -3,7 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def get_go_overlap_matrix(df_uniprot_goa:pd.DataFrame, exclude_iea:bool):
+
+def get_go_overlap_matrix(df_uniprot_goa: pd.DataFrame, exclude_iea: bool):
     df_uniprot_goa_overlap = df_uniprot_goa.copy(deep=True)
     if exclude_iea:
         df_uniprot_goa_overlap = df_uniprot_goa[df_uniprot_goa.evidence_code != "IEA"]
@@ -28,7 +29,49 @@ def get_go_overlap_matrix(df_uniprot_goa:pd.DataFrame, exclude_iea:bool):
     )
     return df_go_overlaps
 
-def plot_go_overlap_matrix(df_go_overlaps:pd.DataFrame, df_uniprot_goa:pd.DataFrame):
+
+def get_overlap_plot(
+    subset, df_go_overlaps, go_id_to_term, filename=None, shorten_labels=False
+):
+    # the percentage of proteins annotated with GO term 1 (x axis) that is also annotated with GO term2
+    best_subset_overlaps = df_go_overlaps.loc[subset, subset].rename(
+        columns=go_id_to_term, index=go_id_to_term
+    )
+    best_subset_overlaps = best_subset_overlaps.loc[
+        sorted(best_subset_overlaps.index), sorted(best_subset_overlaps.columns)
+    ]
+
+    best_subset_overlaps = best_subset_overlaps / np.diag(best_subset_overlaps)
+
+    if shorten_labels:
+        shorten_labels_map = {
+            c: c.replace("transmembrane transporter activity", "TTA")
+            for c in best_subset_overlaps.columns
+        }
+        shorten_labels_map.update(
+            {
+                "ATPase activity, coupled to transmembrane movement of ions, rotational mechanism":\
+                      "ATPase activity ... rotational mechanism (GO:0044769)"
+            }
+        )
+        best_subset_overlaps = best_subset_overlaps.rename(
+            columns=shorten_labels_map, index=shorten_labels_map
+        )
+
+    tmp = best_subset_overlaps.copy()
+    np.fill_diagonal(tmp.values, 0)
+    print("median", tmp.median(axis=None).round(2))
+    print("mean", tmp.mean(axis=None).round(2))
+
+    g = sns.heatmap(best_subset_overlaps, annot=True, fmt=".2f")
+    g.set_xlabel("GO term 1")
+    g.set_ylabel("GO term 2")
+    if filename:
+        plt.savefig(filename, dpi=300, bbox_inches="tight")
+    return g
+
+
+def plot_go_overlap_matrix(df_go_overlaps: pd.DataFrame, df_uniprot_goa: pd.DataFrame):
     fig, ax = plt.subplots(figsize=(30, 20))
     go_id_to_term = {
         go_id: go_term
@@ -46,12 +89,19 @@ def plot_go_overlap_matrix(df_go_overlaps:pd.DataFrame, df_uniprot_goa:pd.DataFr
     return sns.heatmap(df_go_overlaps_ge20, ax=ax, annot=True)
 
 
-def get_chebi_overlaps(df_go_chebi:pd.DataFrame, df_uniprot_goa:pd.DataFrame, exclude_iea:bool, primary_input_only:bool = True):
+def get_chebi_overlaps(
+    df_go_chebi: pd.DataFrame,
+    df_uniprot_goa: pd.DataFrame,
+    exclude_iea: bool,
+    primary_input_only: bool = True,
+):
 
     df_go_chebi_overlaps = df_go_chebi.copy(deep=True)
     df_uniprot_goa_chebi_overlap = df_uniprot_goa.copy(deep=True)
     if exclude_iea:
-        df_uniprot_goa_chebi_overlap = df_uniprot_goa_chebi_overlap[df_uniprot_goa_chebi_overlap.evidence_code != "IEA"]
+        df_uniprot_goa_chebi_overlap = df_uniprot_goa_chebi_overlap[
+            df_uniprot_goa_chebi_overlap.evidence_code != "IEA"
+        ]
     # df_go_chebi_overlaps["proteins"] = df_go_chebi_overlaps.go_id.map(go_to_proteins)
 
     if primary_input_only:
